bea8e46e4c665a9a7b2982721d02a5b67753ace6
Kiamehr <kiamehrmoradii@gmail.com>
2026-06-15 00:09:04 +0330

    added functions file for cleaner code formatingg

---
+++ Andrew Ag's ML Specialization/My Labs and Codes/Project/Kaggle Challenges/Functions.py
@@ -0,0 +1,40 @@
+def clean_titanic_data(dataset):
+    """
+    A Function used for cleaning the dataset \
+        from the Titanic surivival prediction Challenge
+    """
+    # Removing unrealated Columns
+    cleaned_data = dataset.drop(columns=["Name"])
+
+    # Changing genders to numbers
+    cleaned_data = cleaned_data.rename(columns={"Sex": "isFemale"})
+
+    cleaned_data["isFemale"] = (cleaned_data["isFemale"] == "female").astype(int)
+
+    # Cleaning age NaN
+
+    age_mean = cleaned_data["Age"].mean().round()
+
+    cleaned_data["Age"] = cleaned_data["Age"].fillna(age_mean)
+
+    # Cleaning the Ticket Column
+
+    ticket_counts = cleaned_data["Ticket"].value_counts()
+    cleaned_data["Ticket Group Size"] = cleaned_data["Ticket"].map(ticket_counts)
+
+    # Removing the Ticket Column
+    cleaned_data = cleaned_data.drop("Ticket", axis="columns")
+
+    # Cleaning Embarked
+
+    cleaned_data["Embarked"] = cleaned_data["Embarked"].fillna("S")
+
+    cleaned_data["Embarked"] = cleaned_data["Embarked"].astype("category").cat.codes
+
+    # Cleaning Cabin Column
+
+    cleaned_data["Cabin"] = cleaned_data["Cabin"].fillna("U")
+
+    cleaned_data["Cabin"] = cleaned_data["Cabin"].astype("category").cat.codes
+
+    return cleaned_data
