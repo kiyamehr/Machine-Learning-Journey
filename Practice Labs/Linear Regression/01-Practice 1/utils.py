a1ce97869eca7bccd98f4cad1ed7d682145948ea
kiyamehr <kiamehrmoradii@gmail.com>
2026-07-11 11:38:32 +0330

    Trained a Really Faulty Model

---
+++ Practice Labs/Linear Regression/01-Practice 1/utils.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def gen_data():
+    np.random.seed(42)
+
+    sizes = np.random.randint(40, 220, 80)
+
+    prices = 900 * sizes + np.random.normal(0, 20_000, len(sizes))
+    prices = np.round(prices, 0).astype(int)
+
+    x = sizes.reshape(-1, 1)
+    y = prices
+
+    return x, y
