c52d9d8b98c65df04f199ef641f34d37b915701e
kiyamehr <kiamehrmoradii@gmail.com>
2026-07-12 11:25:20 +0330

    started a polynomial problem

---
+++ Practice Labs/Linear Regression/02-Practice 2/utils.py
@@ -0,0 +1,16 @@
+import numpy as np # type: ignore
+
+def gen_data():
+    np.random.seed(12)
+
+    x = np.linspace(-6,6,100)
+
+    y = (
+        np.cos(x)
+        + x*0.15
+        + np.random.normal(0,0.2,100)
+    )
+
+    X = x.reshape(-1,1)
+    
+    return X, y
